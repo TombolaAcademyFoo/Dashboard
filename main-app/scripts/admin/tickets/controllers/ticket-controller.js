fb0e6f598d78e3feb390350b2fc28da5f5222006
Dominic Waterson <dominic.waterson@tombola.com>
2016-01-08 10:06:11 +0000

    refactored the controller

--- main-app/scripts/admin/tickets/controllers/ticket-controller.js
+++ main-app/scripts/admin/tickets/controllers/ticket-controller.js
@@ -3,20 +3,5 @@
     angular.module('Tombola.Academy.Dash.Admin.Tickets')
         .controller('TicketController', ['$scope', 'TicketService', function($scope, ticketService) {
             $scope.ticketService = ticketService;
-
-            $scope.updateTicket = function (ticket) {
-                ticketService.updateTicket(ticket);
-            };
-
-            $scope.removeTicket = function (id) {
-                ticketService.removeTicket(id);
-            };
-
-            $scope.addTicket = function () {
-                if(!ticketService.newTicketFromUi.ticket || !ticketService.newTicketFromUi.gameid){
-                    return;
-                }
-                ticketService.addTicket();
-            };
         }]);
 })();
